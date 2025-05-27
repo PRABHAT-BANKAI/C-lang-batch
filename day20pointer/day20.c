6acac374db7d35c1d56cd71b4d5b46ebbccf1d4a
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-05-27 17:46:27 +0530

    pointer

---
+++ day20pointer/day20.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+
+  // int num1 = 9;
+
+  // float num2 = 5.55555;
+
+  // char str = 'A';
+
+  // printf("Size in bytes: %zu\n", sizeof(num1));
+  // printf("Size in bytes: %zu\n", sizeof(num2));
+  // printf("Size in bytes: %d\n", sizeof(str));
+
+  int *p;
+  int *p2;
+    int *p3;
+
+  int num = 55;
+  int num2 = 99;
+  int num3 = 39;
+
+  p = &num;
+
+  p2 = &num2;
+    p3 = &num3;
+
+  printf("Address is: %u\n", p+1);//scale of pointer
+  printf("Address is: %u\n", p2+1);
+   printf("Address is: %u\n", p3);
+
+  return 0;
+}
