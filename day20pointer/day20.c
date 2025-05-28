76e123936202b0ad25d073c88c646ba8b997410c
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-05-28 17:19:27 +0530

    arrayofPointer

--- day20pointer/day20.c
+++ day20pointer/day20.c
@@ -15,7 +15,7 @@ int main()
 
   int *p;
   int *p2;
-    int *p3;
+  int *p3;
 
   int num = 55;
   int num2 = 99;
@@ -24,11 +24,23 @@ int main()
   p = &num;
 
   p2 = &num2;
-    p3 = &num3;
+  p3 = &num3;
 
-  printf("Address is: %u\n", p+1);//scale of pointer
-  printf("Address is: %u\n", p2+1);
-   printf("Address is: %u\n", p3);
+  printf("Address is: %u\n", p + 1); // scale of pointer
+  printf("Address is: %u\n", p2 + 1);
+  printf("Address is: %u\n", p3);
+
+  int *ptr;
+
+  int arr[5] = {1, 22, 44, 4, 5};
+
+  ptr = &arr[0];
+
+  for (int i = 0; i < 5; i++)
+  {
+
+    printf("%u => %d\n", ptr + i, *(ptr + i));
+  }
 
   return 0;
 }
