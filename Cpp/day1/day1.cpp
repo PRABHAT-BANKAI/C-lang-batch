d5716357af3eb8a612de24406e1a006f40cefa43
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-06-04 09:42:22 +0530

    cppday1

---
+++ Cpp/day1/day1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+// #include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+
+  // int number = 12;
+  // char str[] = "batman"; // string
+
+  // cout << "hello world\n";
+  // cout << number;
+
+  // cout << number << str;
+  // string name;
+  // cout << "Enter Your name:";
+  // cin >> name;
+
+  // cout << "hello"<< "  " << name << "  is  a greate super hero ";
+
+  // for (int i = 2000; i <= 3000; i++)
+  // {
+
+  //   // cout << i << endl;
+
+  //   if (i % 4 == 0)
+  //   {
+  //     cout << i << " ";
+  //   }
+
+
+  // }
+
+
+      int a[5] = {12, 25, 6, 78, 33};
+
+    for (int i = 0; i < 5; i++)
+    {
+      cout << a[i] << "=>" << i << endl;
+    }
+
+  return 0;
+}
